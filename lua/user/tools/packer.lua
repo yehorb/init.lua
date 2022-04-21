06b791160d245ed0df5e15b0ff2a55586a6b7068
Yehor Borkov <yehor.borkov@gmail.com>
2022-04-21 14:41:22 +0300

    feat: added comments to packer install tool

--- lua/user/tools/packer.lua
+++ lua/user/tools/packer.lua
@@ -1,8 +1,10 @@
 local packer = {}
 
+packer.BOOTSTRAP = 0
 packer.INSTALL_PATH = "/site/pack/packer/start/packer.nvim"
 packer.REPOSITORY = "https://github.com/wbthomason/packer.nvim"
 
+--- ensure_install makes sure that packer.nvim is installed.
 function packer.ensure_install()
   if packer.is_installed() then
     return
@@ -11,16 +13,27 @@ function packer.ensure_install()
   vim.notify("Installed packer.nvim. Restart Neovim.", vim.log.levels.WARN, {})
 end
 
+--- is_installed checks if expected packer.nvim installation directory exists
 function packer.is_installed()
   return vim.fn.empty(vim.fn.glob(packer.install_path())) == 0
 end
 
+--- install_path computes expected packer.nvim installation directory
 function packer.install_path()
   return vim.fn.stdpath("data") .. packer.INSTALL_PATH
 end
 
+--- install clones packer.nvim git repository into the expected installation
+--- directory
 function packer.install()
-  vim.fn.system { "git", "clone", "--depth", "1", packer.REPOSITORY, packer.INSTALL_PATH }
+  packer.BOOTSTRAP = vim.fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    packer.REPOSITORY,
+    packer.install_path()
+  }
 end
 
 return packer
