80daa66675e2b5fc7947fdf3f8ab18699974b4ef
Yehor Borkov <yehor.borkov@gmail.com>
2022-04-21 15:36:50 +0300

    feat: packer.nvim configuration code
    Refactored tools.packer module.
    Added install_autosync function.
    Added packer.nvim as a plugin.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,1 +1,14 @@
 require("user.tools.packer").ensure_install()
+
+local ok, packer = pcall(require, "packer")
+if not ok then
+  vim.notify("Error loading packer.nvim", vim.log.levels.ERROR, {})
+end
+
+require("user.tools.packer").install_autosync(packer, "plugins.lua")
+
+packer.init()
+
+return packer.startup(function(use)
+  use "wbthomason/packer.nvim"
+end)
--- lua/user/tools/packer.lua
+++ lua/user/tools/packer.lua
@@ -1,39 +1,59 @@
-local packer = {}
+local M = {}
 
-packer.BOOTSTRAP = 0
-packer.INSTALL_PATH = "/site/pack/packer/start/packer.nvim"
-packer.REPOSITORY = "https://github.com/wbthomason/packer.nvim"
+M.BOOTSTRAP = 0
+M.INSTALL_PATH = "/site/pack/packer/start/packer.nvim"
+M.REPOSITORY = "https://github.com/wbthomason/packer.nvim"
 
 --- ensure_install makes sure that packer.nvim is installed.
-function packer.ensure_install()
-  if packer.is_installed() then
+function M.ensure_install()
+  if M.is_installed() then
     return
   end
-  packer.install()
+  M.install()
   vim.notify("Installed packer.nvim. Restart Neovim.", vim.log.levels.WARN, {})
 end
 
---- is_installed checks if expected packer.nvim installation directory exists
-function packer.is_installed()
-  return vim.fn.empty(vim.fn.glob(packer.install_path())) == 0
+--- is_installed checks if expected packer.nvim installation directory exists.
+function M.is_installed()
+  return vim.fn.empty(vim.fn.glob(M.install_path())) ~= 1
 end
 
---- install_path computes expected packer.nvim installation directory
-function packer.install_path()
-  return vim.fn.stdpath("data") .. packer.INSTALL_PATH
+--- install_path computes expected packer.nvim installation directory.
+function M.install_path()
+  return vim.fn.stdpath("data") .. M.INSTALL_PATH
 end
 
 --- install clones packer.nvim git repository into the expected installation
---- directory
-function packer.install()
-  packer.BOOTSTRAP = vim.fn.system {
+--- directory.
+function M.install()
+  M.BOOTSTRAP = vim.fn.system {
     "git",
     "clone",
     "--depth",
     "1",
-    packer.REPOSITORY,
-    packer.install_path()
+    M.REPOSITORY,
+    M.install_path()
   }
 end
 
-return packer
+
+--- install_autosync creates autocommand to trigger packer.sync() on plugins
+--- defintion file edit.
+-- @param packer packer: Loaded packer.nvim module.
+-- @param plugins_file string: Plugin defintion file name.
+function M.install_autosync(packer, plugins_file)
+  local group_name = "packer_refresh"
+  vim.api.nvim_create_augroup(group_name, { clear = true })
+  vim.api.nvim_create_autocmd("BufWritePost", {
+    group = group_name,
+    pattern = plugins_file,
+    desc = "Trigger packer.sync() on plugins defintion file edit.",
+    callback = function(event)
+      packer.reset()
+      vim.cmd(string.format("luafile %s", event.file))
+      packer.sync()
+    end
+  })
+end
+
+return M
