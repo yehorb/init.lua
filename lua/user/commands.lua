567d46f8ab4d0a458f63d84c5d2510ff6b20a79e
Yehor Borkov <yehor.borkov@gmail.com>
2022-04-30 00:32:09 +0300

    refactor: tools.cwhelp cleanup

--- lua/user/commands.lua
+++ lua/user/commands.lua
@@ -3,5 +3,5 @@ vim.cmd "filetype on"
 vim.cmd "syntax enable"
 
 -- Current word help command
-require("user.tools.cwhelp").install_command()
-require("user.tools.cwhelp").install_keymap()
+require("user.tools.cwhelp"):install_command()
+require("user.tools.cwhelp"):install_keymap()
--- lua/user/tools/cwhelp.lua
+++ lua/user/tools/cwhelp.lua
@@ -1,24 +1,24 @@
-local M = {}
+local cwhelp = {}
 
 --- help opens vim help pane for the word under the cursor, quoted.
-function M.help()
-  local word = vim.fn.epand("<cword>")
+function cwhelp.help(self)
+  local word = vim.fn.expand("<cword>")
   local help_command = string.format("help '%s'", word)
   ok = pcall(vim.api.nvim_command, help_command)
   if not ok then
-    local error_message = string.format("Sorry, no help for '%s'", word)
+    local error_message = string.format("Sorry, no help for '%s'.", word)
     vim.notify_once(error_message, vim.log.levels.ERROR, {})
   end
 end
 
 --- install_command creates CwHelp vim command.
-function M.install_command()
-  vim.api.nvim_create_user_command("CwHelp", M.help, {})
+function cwhelp.install_command(self)
+  vim.api.nvim_create_user_command("CwHelp", self.help, {})
 end
 
 --- install_keymap created keymap to CwHelp command.
-function M.install_keymap()
+function cwhelp.install_keymap(self)
   vim.keymap.set("n", "<leader>ch", ":CwHelp<CR>")
 end
 
-return M
+return cwhelp
