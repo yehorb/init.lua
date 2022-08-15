586d1ae6784aa583fcd6639695a5aab4f11b5f91
Yehor Borkov <yehor.borkov@gmail.com>
2022-08-15 23:09:34 +0300

    feat: essential options

---
+++ lua/user/options.lua
@@ -0,0 +1,15 @@
+local options = {
+  fileencoding = "utf-8",
+  fileformat = "unix",
+  fileformats = { "unix", "dos" },
+  list = true,
+  listchars = { eol = "¶", tab = "→·", trail = "·", nbsp = "+" },
+}
+
+if vim.fn.has("termguicolors") then
+  options.termguicolors = true
+end
+
+for option, value in pairs(options) do
+  vim.opt[option] = value
+end
